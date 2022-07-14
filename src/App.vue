c61cae431d73e161426f433ecc7dcd339cf82cb7
llb <llb131452@qq.com>
2022-07-14 15:28:59 +0800

    '初始化项目版本'

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>00
+<home></home>
+  <router-link to="home">主页</router-link>
+  <router-view></router-view>
+</template>
+<script>
+import home from "@/views/Home";
+export default {
+  components:{
+    home
+  }
+}
+</script>
+<style>
+
+</style>
